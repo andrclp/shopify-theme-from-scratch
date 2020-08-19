3cd733f784b487319537357e193b7b72f4c5d1bd
andrei <andrei_clep@yahoo.com>
2020-08-19 17:10:00 +0300

    jQuery and Cart Item Counter code

--- assets/theme.js
+++ assets/theme.js
@@ -1,1 +1,18 @@
-// Put your applicaiton javascript here
+$(document).ready(function() {
+
+	// CART ITEM COUNTER
+	var cartItemCounter = document.querySelector('span.cart-item-count')
+
+	if (cartItemCounter) {
+		jQuery.ajax({
+			url: "/cart.js",
+			dataType: "json"
+		
+		}).done(function(data) {
+			var newCount = data.item_count
+			cartItemCounter.innerText = newCount
+		})
+	}
+	// END CART ITEM COUNTER
+
+})
